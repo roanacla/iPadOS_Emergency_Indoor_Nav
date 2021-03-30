fb50f84086103c7242abaf3db79749751caa692e
roanacla <roanacla@me.com>
2021-03-29 17:49:17 -0700

    Create Xcode Project

---
+++ iPadOS_Emergency_Indoor_Nav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iPadOS_Emergency_Indoor_Nav
+//
+//  Created by Roger Navarro on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
